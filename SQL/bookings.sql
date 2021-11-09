8aca451c701f1087950b333b5872f4ab065e09cd
pauui <erikapaulalim26@gmail.com>
2021-11-09 15:55:01 +0800

    database: bookingcalendar

--- SQL/bookings.sql
+++ SQL/bookings.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 05, 2021 at 06:49 AM
+-- Generation Time: Nov 09, 2021 at 08:49 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.3.31
 
@@ -30,10 +30,12 @@ SET time_zone = "+00:00";
 CREATE TABLE `bookings` (
   `id` int(11) NOT NULL,
   `date` date NOT NULL,
-  `timeslot` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `contact` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `timeslot` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `guest` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL
+  `contact` varchar(255) NOT NULL,
+  `account` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
